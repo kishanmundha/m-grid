5dd6ab943dba9bd8142966e52b8b6a2c8601aad5
Kishan Mundha <kishan.mundha@gmail.com>
2017-05-24 13:35:04 +0530

    Issue #21 resolved

---
+++ src/m-grid.service.js
@@ -0,0 +1,65 @@
+'use strict';
+
+angular.module('m-grid.service', [])
+
+.service('mGridService', ['$log', function ($log) {
+    /**
+     * Generate template for grid
+     * @public
+     * @param {*} gridOptions
+     * @param {*} mGridConfig
+     * @return {String} Grid template
+     */
+    function getGridTemplate (gridOptions, mGridConfig) {
+        var html = '';
+
+        html += '<div style="overflow-x:auto;width: 100%;">';
+        html += '<table class="' + mGridConfig.tableClass + '">';
+        html += '<thead>';
+        html += '<tr>';
+        html += '<th ng-repeat="column in gridOptions.columns" class="' + mGridConfig.thClass + '">';
+        html += '<span ng-bind="column.name">Name</a>';
+        html += '</th>';
+        html += '</tr>';
+        html += '</thead>';
+        html += '<tbody>';
+        html += '<tr ng-repeat="item in gridOptions.data">';
+
+        angular.forEach(gridOptions.columns, function (item) {
+            html += getCellTemplate(item);
+        });
+
+        html += '</tr>';
+        html += '</tbody>';
+        html += '</thead>';
+        html += '</table>';
+        html += '</div>';
+
+        return html;
+    }
+
+    /**
+     * Generate template for cell
+     * @private
+     * @param {*} column
+     * @return {String} Cell template
+     */
+    function getCellTemplate (column) {
+        var cellTemplate = '<td>';
+
+        if (column.cellTemplate) {
+            cellTemplate += '<div ng-init="row={\'entity\':item}">' + column.cellTemplate + '</div>';
+        } else {
+            cellTemplate += '<span ng-bind="item[\'' + column.field + '\']"></span>';
+        }
+
+        cellTemplate += '</td>';
+
+        return cellTemplate;
+    }
+
+    return {
+        getGridTemplate: getGridTemplate,
+        getCellTemplate: getCellTemplate
+    };
+}]);
---
+++ test/m-grid.service.spec.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+
+'use strict';
+
+describe('m-grid.service', function () {
+    var mGridService;
+    beforeEach(module('m-grid.service'));
+
+    beforeEach(inject(function (_mGridService_) {
+        mGridService = _mGridService_;
+    }));
+
+    it('m-grid.service getGridTemplate', function () {
+        var gridOptions = {
+            columns: [{
+                name: '#',
+                field: 'id'
+            }, {
+                name: 'column1',
+                field: 'column1'
+            }]
+        };
+
+        var mGridConfig = {
+            tableClass: 'my-table-class',
+            thClass: 'my-th-class'
+        };
+        var template = mGridService.getGridTemplate(gridOptions, mGridConfig);
+
+        expect(template).toBe('<div style="overflow-x:auto;width: 100%;"><table class="my-table-class"><thead><tr><th ng-repeat="column in gridOptions.columns" class="my-th-class"><span ng-bind="column.name">Name</a></th></tr></thead><tbody><tr ng-repeat="item in gridOptions.data"><td><span ng-bind="item[\'id\']"></span></td><td><span ng-bind="item[\'column1\']"></span></td></tr></tbody></thead></table></div>');
+    });
+
+    it('m-grid.service getCellTemplate', function () {
+        var column = {
+            name: '#',
+            field: 'id',
+            cellTemplate: '<span>my-custom-template</span>'
+        };
+
+        var template = mGridService.getCellTemplate(column);
+
+        expect(template).toBe('<td><div ng-init="row={\'entity\':item}"><span>my-custom-template</span></div></td>');
+    });
+});
